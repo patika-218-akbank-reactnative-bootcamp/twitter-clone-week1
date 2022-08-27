6ca106f8137dd16cb2f650bee6a84f333c6c2238
Enes Ozturk <enesozturk.d@gmail.com>
2022-08-27 14:40:50 +0300

    feat: add absolute path

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,16 @@
 module.exports = {
-  arrowParens: "avoid",
+  arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
-  trailingComma: "all",
+  trailingComma: 'all',
   printWidth: 80,
+  importOrder: [
+    '^(react|react-native)$',
+    '<THIRD_PARTY_MODULES>',
+    '^@twitter/',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@twitter/assets': './src/assets',
+          '@twitter/components': './src/components',
+          '@twitter/constants': './src/constants',
+          '@twitter/navigation': './src/navigation',
+          '@twitter/hooks': './src/hooks',
+          '@twitter/provider': './src/provider',
+          '@twitter/screens': './src/screens',
+          '@twitter/screens': './src/screens',
+        },
+      },
+    ],
+  ],
 };
